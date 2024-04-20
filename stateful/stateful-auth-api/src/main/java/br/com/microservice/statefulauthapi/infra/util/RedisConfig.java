ea85f6eba29a66a798390deb7fda83dea583ed35
luan nascimento <almeida.luan099@gmail.com>
2024-04-20 18:01:11 -0300

    Configuracao do redis no projeto stateful

---
+++ stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/infra/util/RedisConfig.java
@@ -0,0 +1,30 @@
+package br.com.microservice.statefulauthapi.infra.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost,redisPort));
+    }
+
+    @Bean
+    RedisTemplate<String,String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String,String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+}
